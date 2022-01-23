a91565de6ef5564b521767469f1736aa5386b0c2
AsmitaChakraborty <86459479+AsmitaChakrabortyy@users.noreply.github.com>
2022-01-23 12:09:57 +0530

    Add files via upload

---
+++ edtechqnabot-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\edtechqnabot-bot-ae5d-Web-Deploy.pubxml -p:Password=Jpa5Q7veBRJgWezyWA7rmpgks33ZLS5T6vfjwMc9qi42k2LY6Plpn9mH4eMo
+
